7c758db4ff5de4b7834c77bd795df488488588a7
Darya <dareivolkova@mail.ru>
2018-11-30 13:40:06 +0300

    MY MESSAGE OHH MY GAWD

--- src/clickCounter/script.js
+++ src/clickCounter/script.js
@@ -1,22 +1,48 @@
 const clickBtn = document.getElementById('click-btn')
+const resetBtn = document.getElementById('reset-btn')
+const increaseStepBtn = document.getElementById('increaseStep-btn')
+const increaseMaxBtn = document.getElementById('increaseMax-btn')
 const clickValue = document.getElementById('click-value')
-const clickReset = document.getElementById('click-reset')
+const maxValue = document.getElementById('max-value')
+const LeftValue = document.getElementById('left-value')
+const clickStep = document.getElementById('click-step')
+
+const TWENTY = 20
 
 let clicks = 0
 let step = 1
-const maxValue = 10
+let maxNumber = TWENTY
+maxValue.innerHTML = maxNumber
+LeftValue.innerHTML = maxNumber - clicks
 
 clickBtn.onclick = function() {
-	if (clicks > maxValue) {
+	if (clicks < maxNumber) {
 		clicks = clicks + step
 	} else {
-		clicks = 'max value reached'
+		maxValue.innerHTML = 'max value reached'
 	}
 
 	clickValue.innerHTML = clicks
+
+	LeftValue.innerHTML = maxNumber - clicks
 }
 
-clickBtn.onclick = function() {
+resetBtn.onclick = function() {
 	clicks = 0
 	clickValue.innerHTML = clicks
+	maxValue.innerHTML = TWENTY
+	clickStep.innerHTML = 1
+	LeftValue.innerHTML = TWENTY
+}
+
+increaseStepBtn.onclick = function() {
+	step = step + 1
+
+	clickStep.innerHTML = step
+}
+
+increaseMaxBtn.onclick = function() {
+	maxNumber = maxNumber + 1
+
+	maxValue.innerHTML = maxNumber
 }
---
+++ src/main/script.js
@@ -0,0 +1,35 @@
+// const container = document.getElementById('container')
+// const features = [
+// 	{ label: 'Broascasts' },
+// 	{ label: 'Broascasts' },
+// 	{ label: 'Broascasts' },
+// 	{ label: 'Broascasts' },
+// ]
+
+// //  'Flows', 'Template', 'Keyword']
+
+// // function greet(name) {
+// // 	return 'Hello,' + name + '!'
+// // }
+
+// // function canDrink(age) {
+// // 	return age >= 18
+// // }
+
+// // const greeting = greet('Dima')
+
+// const person = { age: 27, name: 'Dima' }
+// console.log('person', person)
+
+// features.forEach(function(item) {
+// 	const feature = document.createElement('div')
+// 	const icon = document.createElement('div')
+
+// 	icon.classList.add('text-center')
+// 	feature.classList.add('card', 'card-narrow', 'standard-border', 'm-t')
+// 	feature.innerHTML = item.label
+// 	icon.innerHTML = item.icon
+
+// 	feature.append(icon)
+// 	container.append(feature)
+// })
